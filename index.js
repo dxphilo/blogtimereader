90a9cf25257927a7f7b49f630ba6ecf538485f3f
John Philip <developerphilo@gmail.com>
2021-10-10 09:22:14 -0700

    updated changes for effectivity

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 module.exports=function blogReadTime(){
         const el = document.getElementById('blog-readtime').textContent.replace(/['".:;]+/g,'').split(" ");
         let cleanString=el.filter(s => s.replace(/\s+/g, '').length !== 0);
-        let totalWords=[]
+        const totalWords=[]
         totalWords.push(cleanString)
-        console.log(totalWords)
-        let words=totalWords[0].length
+        const words=totalWords[0].length
         // 1 words => approximately 0.4390243902439024 seconds
-        let readTime=((words*0.4390243902439024)/60).toFixed(1)
+        let readTime=Math.floor((words*0.4390243902439024)/60)
         if(readTime >=1){
-            return (`${readTime} min read`)
+            return readTime
         }else{
           return 0
         }
